a8c75ac5365f1ebdd1a7ce7dfd54e77123cba925
kiril <kirillkravcenko029@gmail.com>
2022-11-25 21:47:09 +0300

    adder in nav

--- app/src/main/java/com/example/masangir/Nav.java
+++ app/src/main/java/com/example/masangir/Nav.java
@@ -1,10 +1,14 @@
 package com.example.masangir;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -12,10 +16,16 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.masangir.databinding.ActivityNavigationBinding;
 
+import android.view.View;
+import android.widget.TextView;
+
 public class Nav extends AppCompatActivity {
+    private LinearLayout mLayout;
+    private EditText mEditText;
+    private Button mButton;
+    public String text;
 
     private ActivityNavigationBinding binding;
-    //6666666
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +35,37 @@ public class Nav extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_navigation);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        // Создание нового чата
+        setContentView(R.layout.activity_navigation);
+        mLayout = (LinearLayout) findViewById(R.id.container);
+        mEditText = (EditText) findViewById(R.id.editText);
+        mButton = (Button) findViewById(R.id.button);
+        TextView textView = new TextView(this);
+        textView.setText("New text");
+        text = String.valueOf(mEditText.getText());
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLayout.addView(createNewTextView(mEditText.getText().toString()));
+
+            }
+        });
+    }
+
+    private TextView createNewTextView(String text) {
+        final LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        final TextView textView = new TextView(this);
+        textView.setLayoutParams(lparams);
+        textView.setText("New text: " + text);
+        return textView;
     }
 
 
