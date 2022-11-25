a97cfd6e0e1d0a5e328a164ebdc078ce3c523d7d
Enot1she <enotisheRND@yandex.ru>
2022-11-25 18:18:09 +0300

    com2

--- app/src/main/java/com/example/masangir/MainActivity.java
+++ app/src/main/java/com/example/masangir/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.masangir;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class Register extends AppCompatActivity {
+
+    public Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
+
+        btn = (Button) findViewById(R.id.Reg);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this, Nav.class);
+                startActivity(intent);
+            }
+        });
     }
 }
