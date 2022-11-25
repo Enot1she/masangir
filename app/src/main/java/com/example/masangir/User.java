04400521316d642f56c82b59603f7f1a590ab05b
HUAWEI <sanchous2810@gmail.com>
2022-11-25 22:53:42 +0300

    user class

---
+++ app/src/main/java/com/example/masangir/User.java
@@ -0,0 +1,16 @@
+package com.example.masangir;
+
+public class User {
+    public int userId;
+    public String userName;
+    public String userPassword;
+
+    public User(int userId, String userName, String userPassword){
+        this.userId = userId;
+        this.userName = userName;
+        this.userPassword = userPassword;
+    }
+
+    public User(){
+    }
+}
